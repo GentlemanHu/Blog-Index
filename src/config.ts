614f206656941e7e86bfc374c9ff6d13cc0f2aa3
Gentleman.Hu <justfeelingme@gmail.com>
2024-01-13 06:51:54 +0000

    add own config

--- src/config.ts
+++ src/config.ts
@@ -1,52 +1,148 @@
 const WEBSITE_SORTS: WebsiteSort[] = [
-  { id: 1, title: "示例分类1" },
-  { id: 2, title: "示例分类2" },
-  { id: 3, title: "示例分类3" },
-  { id: 4, title: "示例分类4" },
-  { id: 5, title: "示例分类5" },
-  { id: 6, title: "友情链接" },
+  {id: 1, title: "我的"},
+  {id: 2, title: "管理"},
+  {id: 3, title: "AI&GPT"},
+  {id: 4, title: "Subtle"},
 ];
 
 const WEBSITE_ITEMS: WebsiteItem[] = [
-  {
-    sortId: 1,
-    title: "主标题",
-    url: "http://www.your-web-page.com/",
-    icon: "",
-    color: "#0171CD",
-  },
-  {
-    sortId: 1,
-    title: "主标题",
-    description: "网站描述",
-    url: "http://www.your-web-page.com/",
-    icon: "",
-  },
-  {
-    sortId: 1,
-    title: "主标题",
-    description: "网站描述",
-    url: "http://www.your-web-page.com/",
-    icon: "",
-  },
-  {
-    sortId: 6,
-    title: "EsunR Blog",
-    description: "EsunR 的博客",
-    url: "https://www.esunr.xyz",
-    icon: "https://esunr-image-bed.oss-cn-beijing.aliyuncs.com/logo.jpg",
-  },
-];
+    {
+      sortId: 1,
+      title: "我的博客",
+      description: "GodLin’s Blog",
+      url: "https://crushing.xyz",
+      icon: "https://img.icons8.com/glyph-neue/128/the-flash-head.png"
+    },
+    {
+      sortId: 1,
+      title: "笔记本",
+      description: "Jupyter Lab",
+      url: "https://jupyter.crushing.xyz",
+      icon: "https://img.icons8.com/nolan/96/spiral-bound-booklet.png"
+    },
+    {
+      sortId: 1,
+      title: "书库",
+      description: "Calibre-Web Library",
+      url: "https://store.crushing.xyz",
+      icon: "https://img.icons8.com/nolan/96/library.png"
+    },
+    {
+      sortId: 2,
+      title: "服务器状态",
+      description: "Status",
+      url: "https://status.crushing.xyz",
+      icon: "https://img.icons8.com/nolan/128/ok.png"
+    },
+    {
+      sortId: 2,
+      title: "站点状态",
+      description: "UpTime",
+      url: "https://up.crushing.xyz",
+      icon: "https://img.icons8.com/nolan/96/monitor.png"
+    },
+    {
+      sortId: 2,
+      title: "Docker管理",
+      description: "Portainer Tecent 25",
+      url: "https://docker.crushing.xyz",
+      icon: "https://img.icons8.com/nolan/128/docker.png"
+    },
+    {
+      sortId: 3,
+      title: "Next Chat",
+      description: "Own ChatGPT",
+      url: "https://gpt.crushing.xyz",
+      icon: "https://img.icons8.com/nolan/128/chatgpt.png"
+    },
+    {
+      sortId: 3,
+      title: "ComfyUI Remote",
+      description: "ComyUI",
+      url: "https://comfy.crushing.xyz",
+      icon: "https://img.icons8.com/external-icongeek26-outline-colour-icongeek26/64/external-Painter-carpentry-tools-icongeek26-outline-colour-icongeek26.png"
+    },
+    {
+      sortId: 3,
+      title: "AI 工作流",
+      description: "AI Flowise",
+      url: "https://aiflow.crushing.xyz",
+      icon: "https://img.icons8.com/color/96/tree-structure.png"
+    },
+    {
+      sortId: 3,
+      title: "Lob Chat",
+      description: "Lob Chat",
+      url: "https://bot.crushing.xyz",
+      icon: "https://img.icons8.com/color/96/tree-structure.png"
+    },
+    {
+      sortId: 3,
+      title: "Gemini Chat",
+      description: "gemini chat",
+      url: "https://gemini.crushing.xyz",
+      icon: "https://img.icons8.com/color/96/tree-structure.png"
+    },
+    {
+      sortId: 3,
+      title: "Ai Web Chat",
+      description: "AI web chat",
+      url: "https://aiweb.crushing.xyz",
+      icon: "https://img.icons8.com/color/96/tree-structure.png"
+    },
+    {
+      sortId: 4,
+      title: "WeChat Blog",
+      description: "WeChat Blog",
+      url: "https://ai.crushing.xyz",
+      icon: "https://img.icons8.com/nolan/128/wordpress.png"
+    },
+    {
+      sortId: 2,
+      title: "FRP 面板",
+      description: "Frp",
+      url: "https://frp.crushing.xyz",
+      icon: "https://img.icons8.com/nolan/96/speed.png"
+    },
+    {
+      sortId: 2,
+      title: "青龙 面板",
+      description: "QL",
+      url: "https://ql.crushing.xyz",
+      icon: "https://img.icons8.com/nolan/96/control-panel.png"
+    },
+    {
+      sortId: 1,
+      title: "个人网盘集合",
+      description: "Alist",
+      url: "https://yun.crushing.xyz",
+      icon: "https://img.icons8.com/nolan/96/cloud.png"
+    },
+    {
+      sortId: 4,
+      title: "GitHub 加速",
+      description: "GitHub Proxy",
+      url: "https://ghproxy.crushing.xyz",
+      icon: "https://img.icons8.com/nolan/128/github.png"
+    },
+    {
+      sortId: 4,
+      title: "Docs 笔记文档",
+      description: "Gitbook",
+      url: "https://docs.crushing.xyz",
+      icon: "https://img.icons8.com/nolan/128/google-docs.png"
+    }
+  ]; // 网站分类列表，请按照API.md中'/getPages'接口中返回的data格式填写
 
 const GLOBAL_CONFIG = {
   /**
    * 博客名称
    */
-  BLOG_NAME: "EsunR-Blog",
+  BLOG_NAME: "主页",
   /**
    * 个人博客链接
    */
-  BLOG_URL: "https://blog.esunr.xyz",
+  BLOG_URL: "https://crushing.xyz",
   /**
    * 指定中心 LOGO 图片地址
    */
@@ -54,7 +150,7 @@ const GLOBAL_CONFIG = {
   /**
    * 个人 Github 链接
    */
-  GITHUB_URL: "https://github.com/EsunR",
+  GITHUB_URL: "https://github.com/GentlemanHu",
   /**
    * 背景图片地址
    */
@@ -62,7 +158,7 @@ const GLOBAL_CONFIG = {
   /**
    * ICP 备案号，留空不显示
    */
-  ICP: "京ICP证000001号",
+  ICP: "豫ICP备2021003027",
   ICP_URL: "https://beian.miit.gov.cn/",
   FOOTER_INFO: true,
   /**
@@ -96,5 +192,3 @@ const GLOBAL_CONFIG = {
     description: "This is my personal blog index page.",
   },
 };
-
-export default GLOBAL_CONFIG;
